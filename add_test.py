bca68a90dc73413662dde4aa5bcc18840f5d3e05
Shiv Sagar <shivsa@amdocs.com>
2020-11-23 09:05:06 +0530

    true chnage

--- add_test.py
+++ add_test.py
@@ -5,4 +5,4 @@ class TestCuboid(unittest.TestCase):
     def test_add(self):
         self.assertAlmostEqual(add(2,6),8)
         self.assertAlmostEqual(add(1.0,0.0),1.0)
-        self.assertAlmostEqual(add(1.0,1.0),1.0)
+        self.assertTrue(add(1.0,1.0),2.0)
